40ce72fb3aa375af48dd6328355d833e06c5c50c
Hugo <hhuzan@gmail.com>
2024-05-05 11:53:47 -0300

    State based on dashboard templete

--- src/pages/State.jsx
+++ src/pages/State.jsx
@@ -2,15 +2,19 @@ import { useState, useEffect } from "react";
 import { getStations } from "../utils/getStations";
 import { getStatus } from "../utils/getStatus";
 import { Tarjeta } from "../components/Tarjeta";
-import { appFirebase } from "../utils/conexionAPIFirebase";
-import { getAuth, signOut } from "firebase/auth";
 import { getFavorites } from "../utils/getFavorites";
-import { Box, Typography, Container, CircularProgress, AppBar, IconButton } from "@mui/material";
-import SettingsIcon from "@mui/icons-material/Settings";
-import LogoutIcon from "@mui/icons-material/Logout";
-import PasswordIcon from "@mui/icons-material/Password";
+import { Box, Typography, Container, CircularProgress, IconButton } from "@mui/material";
 
-const auth = getAuth(appFirebase);
+import { styled } from "@mui/material/styles";
+import MuiDrawer from "@mui/material/Drawer";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import Grid from "@mui/material/Grid";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import { mainListItems, secondaryListItems } from "./listItems";
 
 export const State = ({ usuario }) => {
 	const [stations, setStations] = useState([]);
@@ -34,27 +38,6 @@ export const State = ({ usuario }) => {
 		}
 	}, [stations, status]);
 
-	const Header = () => {
-		return (
-			<AppBar
-				position="sticky"
-				sx={{ display: "flex", flexFlow: "row", justifyContent: "space-between", alignItems: "center" }}
-			>
-				<Typography variant="h3">Bici CaC</Typography>
-				<Typography variant="h6">{usuario.email}</Typography>
-				<IconButton size="large" href="/config">
-					<SettingsIcon />
-				</IconButton>
-				<IconButton size="large" href="/change-password">
-					<PasswordIcon />
-				</IconButton>
-				<IconButton size="large" onClick={() => signOut(auth)}>
-					<LogoutIcon />
-				</IconButton>
-			</AppBar>
-		);
-	};
-
 	const Body = () => {
 		return isLoading1 || isLoading2 ? (
 			<Box sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
@@ -76,10 +59,124 @@ export const State = ({ usuario }) => {
 		);
 	};
 
+	const drawerWidth = 240;
+	const AppBar = styled(MuiAppBar, {
+		shouldForwardProp: (prop) => prop !== "open",
+	})(({ theme, open }) => ({
+		zIndex: theme.zIndex.drawer + 1,
+		transition: theme.transitions.create(["width", "margin"], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen,
+		}),
+		...(open && {
+			marginLeft: drawerWidth,
+			width: `calc(100% - ${drawerWidth}px)`,
+			transition: theme.transitions.create(["width", "margin"], {
+				easing: theme.transitions.easing.sharp,
+				duration: theme.transitions.duration.enteringScreen,
+			}),
+		}),
+	}));
+
+	const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== "open" })(({ theme, open }) => ({
+		"& .MuiDrawer-paper": {
+			position: "relative",
+			whiteSpace: "nowrap",
+			width: drawerWidth,
+			transition: theme.transitions.create("width", {
+				easing: theme.transitions.easing.sharp,
+				duration: theme.transitions.duration.enteringScreen,
+			}),
+			boxSizing: "border-box",
+			...(!open && {
+				overflowX: "hidden",
+				transition: theme.transitions.create("width", {
+					easing: theme.transitions.easing.sharp,
+					duration: theme.transitions.duration.leavingScreen,
+				}),
+				width: theme.spacing(7),
+				[theme.breakpoints.up("sm")]: {
+					width: theme.spacing(9),
+				},
+			}),
+		},
+	}));
+
+	const [open, setOpen] = useState(true);
+	const toggleDrawer = () => {
+		setOpen(!open);
+	};
+
 	return (
-		<Container>
-			<Header />
-			<Body />
-		</Container>
+		<Box sx={{ display: "flex" }}>
+			<AppBar position="absolute" open={open}>
+				<Toolbar
+					sx={{
+						pr: "24px", // keep right padding when drawer closed
+					}}
+				>
+					<IconButton
+						edge="start"
+						color="inherit"
+						aria-label="open drawer"
+						onClick={toggleDrawer}
+						sx={{
+							marginRight: "36px",
+							...(open && { display: "none" }),
+						}}
+					>
+						<MenuIcon />
+					</IconButton>
+					<Typography component="h1" variant="h4" color="inherit" noWrap sx={{ flexGrow: 3 }}>
+						Status
+					</Typography>
+					{/* <IconButton color="inherit">
+						<Badge badgeContent={4} color="secondary">
+							<NotificationsIcon />
+						</Badge>
+					</IconButton> */}
+					<Typography component="h1" variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+						{usuario.email}
+					</Typography>
+				</Toolbar>
+			</AppBar>
+			<Drawer variant="permanent" open={open}>
+				<Toolbar
+					sx={{
+						display: "flex",
+						alignItems: "center",
+						justifyContent: "flex-end",
+						px: [1],
+					}}
+				>
+					<IconButton onClick={toggleDrawer}>
+						<ChevronLeftIcon />
+					</IconButton>
+				</Toolbar>
+				<Divider />
+				<List component="nav">
+					{mainListItems}
+					{/* <Divider sx={{ my: 1 }} /> */}
+					{/* {secondaryListItems} */}
+				</List>
+			</Drawer>
+			<Box
+				component="main"
+				sx={{
+					backgroundColor: (theme) =>
+						theme.palette.mode === "light" ? theme.palette.grey[100] : theme.palette.grey[900],
+					flexGrow: 1,
+					height: "100vh",
+					overflow: "auto",
+				}}
+			>
+				<Toolbar />
+				<Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+					<Grid container spacing={3}>
+						<Body />
+					</Grid>
+				</Container>
+			</Box>
+		</Box>
 	);
 };
