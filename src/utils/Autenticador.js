8042434b0a54eb9a82e7ec970bf5629a5883be34
Hugo <hhuzan@gmail.com>
2024-05-12 20:51:32 -0300

    -

--- src/utils/Autenticador.js
+++ src/utils/Autenticador.js
@@ -1,69 +1,67 @@
 import {
-  getAuth,
-  onAuthStateChanged,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  sendPasswordResetEmail,
-  updatePassword,
+	getAuth,
+	onAuthStateChanged,
+	signInWithEmailAndPassword,
+	createUserWithEmailAndPassword,
+	sendPasswordResetEmail,
+	updatePassword,
 } from "firebase/auth";
 import manejoErrores from "./manejoErrores";
 import { appFirebase } from "./conexionAPIFirebase";
 
 class Autenticador {
-  constructor(setIsLoggedIn) {
-    this.appFirebase = appFirebase;
-    this.auth = getAuth(this.appFirebase);
-    this.setIsLoggedIn = setIsLoggedIn;
-    onAuthStateChanged(this.auth, (usuarioFirebase) => {
-      setIsLoggedIn(!!usuarioFirebase);
-    });
-  }
+	constructor(setIsLoggedIn) {
+		this.appFirebase = appFirebase;
+		this.auth = getAuth(this.appFirebase);
+		this.setIsLoggedIn = setIsLoggedIn;
+		onAuthStateChanged(this.auth, (usuarioFirebase) => {
+			setIsLoggedIn(!!usuarioFirebase);
+		});
+	}
 
-  async login(email, password) {
-    try {
-      await signInWithEmailAndPassword(this.auth, email, password);
-    } catch (error) {
-      throw manejoErrores(error.code, error.message);
-    }
-  }
+	async login(email, password) {
+		try {
+			await signInWithEmailAndPassword(this.auth, email, password);
+		} catch (error) {
+			throw manejoErrores(error.code, error.message);
+		}
+	}
 
-  async changePassword(password) {
-    try {
-      await updatePassword(this.auth.currentUser, password);
-    } catch (error) {
-      throw manejoErrores(error.code, error.message);
-    }
-  }
+	async changePassword(password) {
+		try {
+			await updatePassword(this.auth.currentUser, password);
+		} catch (error) {
+			throw manejoErrores(error.code, error.message);
+		}
+	}
 
-  async passwordReset(email) {
-    try {
-      await sendPasswordResetEmail(this.auth, email);
-    } catch (error) {
-      throw manejoErrores(error.code, error.message);
-    }
-  }
+	async passwordReset(email) {
+		try {
+			await sendPasswordResetEmail(this.auth, email);
+		} catch (error) {
+			throw manejoErrores(error.code, error.message);
+		}
+	}
 
-  async register(email, password) {
-    try {
-      await createUserWithEmailAndPassword(this.auth, email, password);
-    } catch (error) {
-      throw manejoErrores(error.code, error.message);
-    }
-  }
+	async register(email, password) {
+		try {
+			await createUserWithEmailAndPassword(this.auth, email, password);
+		} catch (error) {
+			throw manejoErrores(error.code, error.message);
+		}
+	}
 
-  async signOut() {
-    this.auth.signOut();
-  }
+	async signOut() {
+		this.auth.signOut();
+	}
 
-  get uid() {
-    //esto podría dar error?
-    return this.auth.currentUser.uid;
-  }
+	get uid() {
+		return this.auth.currentUser.uid;
+	}
 
-  get email() {
-    //esto podría dar error?
-    return this.auth.currentUser.email;
-  }
+	get email() {
+		return this.auth.currentUser.email;
+	}
 }
 
 export default Autenticador;
