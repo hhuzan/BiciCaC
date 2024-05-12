76babfdc4ae9ba0c7dad82b0c85c3de1013df7d5
Hugo <hhuzan@gmail.com>
2024-05-12 07:23:12 -0300

    update README

--- README.md
+++ README.md
@@ -1,19 +1,35 @@
 # Bici Cac
 
+## Trabajo Práctico Final del Curso de React en Codo-a-Codo 4.0
+
+Aplicación para visualizar en tiempo real el estado de las de [Estaciones Ecobici](https://baecobici.com.ar/) **favoritas** del usuario.
+
+### Instalacion "Local"
+
 -   Copiar el archivo `.env` a `env.local`
 -   Agregar las keys en el archivo `env.local`
 -   `npm install`
 -   `npm run dev`
 
+### Desarrollado con:
+
+-   [React](https://react.dev/)
+-   [Firebase Authentication](https://firebase.google.com/docs/auth)
+-   [Firebase Realtime Database](https://firebase.google.com/docs/database)
+
+### API
+
+-   [API Transporte](https://api-transporte.buenosaires.gob.ar/) BA Ciudad
+
+### Deploy
+
+-   [Vercel](https://vercel.com/)
+
 ---
 
-El cambio de modo Dark/Default se hace en `App.jsx` (`darkMode = true`)
+Para evitar errorers de CORS, en modo development local, el acceso a la api se hace mediante un PROXY configurado en `vite.config.js`
 
----
-
-Para evitar el error de CORS, en modo development, el acceso a la api se hace mediante un PROXY configurado en `vite.config.js`
-
-En producción se debería deployar un server que haga de gateway a la api.
+El deploy en Vercel utiliza un backend mínimo que hace de gateway a las APIs del gobierno de la Ciudad.
 
 ---
 
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,14 +1,5 @@
 import React, { useContext, useState } from "react";
-import {
-  Avatar,
-  Button,
-  TextField,
-  Grid,
-  Box,
-  Typography,
-  Container,
-  Link,
-} from "@mui/material";
+import { Avatar, Button, TextField, Grid, Box, Typography, Container, Link } from "@mui/material";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import { useNavigate } from "react-router-dom";
 import { Copyright } from "../components/CopyRight";
@@ -17,114 +8,90 @@ import AutContext from "../utils/AutContex";
 import { AlertDialog } from "../components/AlertDialog";
 
 export const SignUp = () => {
-  const navigate = useNavigate();
-  const autenticador = useContext(AutContext);
+	const navigate = useNavigate();
+	const autenticador = useContext(AutContext);
 
-  const [openDialog, setOpenDialog] = useState(false);
-  const [dialogContent, setDialogContent] = useState("");
-  const [dialogActionLabel, setDialogActionLabel] = useState("");
+	const [openDialog, setOpenDialog] = useState(false);
+	const [dialogContent, setDialogContent] = useState("");
+	const [dialogActionLabel, setDialogActionLabel] = useState("");
 
-  const handleCloseDialog = () => {
-    setOpenDialog(false);
-  };
+	const handleCloseDialog = () => {
+		setOpenDialog(false);
+	};
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		const data = new FormData(event.currentTarget);
 
-    if (!validarCorreoElectronico(data.get("email"))) {
-      setDialogContent("No es una dirección de correo válida.");
-      setDialogActionLabel("Reintentar");
-      setOpenDialog(true);
-      return;
-    }
+		if (!validarCorreoElectronico(data.get("email"))) {
+			setDialogContent("No es una dirección de correo válida.");
+			setDialogActionLabel("Reintentar");
+			setOpenDialog(true);
+			return;
+		}
 
-    if (data.get("password").length < 8) {
-      setDialogContent(
-        "El largo de la contraseña debe tener una longitud igual o mayor a 8 caracteres."
-      );
-      setDialogActionLabel("Reintentar");
-      setOpenDialog(true);
-      return;
-    }
+		if (data.get("password").length < 8) {
+			setDialogContent("La Contraseña debe tener 8 o más Caracteres.");
+			setDialogActionLabel("Reintentar");
+			setOpenDialog(true);
+			return;
+		}
 
-    try {
-      await await autenticador.register(
-        data.get("email"),
-        data.get("password")
-      );
-      navigate("/");
-    } catch (error) {
-      setDialogContent(error);
-      setDialogActionLabel("Reintentar");
-      setOpenDialog(true);
-    }
-  };
+		try {
+			await await autenticador.register(data.get("email"), data.get("password"));
+			navigate("/");
+		} catch (error) {
+			setDialogContent(error);
+			setDialogActionLabel("Reintentar");
+			setOpenDialog(true);
+		}
+	};
 
-  return (
-    <Container component="main" maxWidth="xs">
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Registrate
-        </Typography>
-        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                id="email"
-                label="Correo electrónico"
-                name="email"
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                name="password"
-                label="Contraseña"
-                type="password"
-                id="password"
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Registrate
-          </Button>
-          <Grid container justifyContent="flex-end">
-            <Grid item>
-              <Link onClick={() => navigate("/")} variant="body2">
-                ¿Ya tiene una cuenta? Iniciar sesión
-              </Link>
-            </Grid>
-          </Grid>
-        </Box>
-      </Box>
-      <Copyright sx={{ mt: 5 }} />
-      <AlertDialog
-        open={openDialog}
-        handleClose={handleCloseDialog}
-        title="Registrate"
-        content={dialogContent}
-        label={dialogActionLabel}
-      />{" "}
-    </Container>
-  );
+	return (
+		<Container component="main" maxWidth="xs">
+			<Box
+				sx={{
+					marginTop: 8,
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+				}}
+			>
+				<Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+					<LockOutlinedIcon />
+				</Avatar>
+				<Typography component="h1" variant="h5">
+					Registrate
+				</Typography>
+				<Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+					<Grid container spacing={2}>
+						<Grid item xs={12}>
+							<TextField required fullWidth id="email" label="Correo electrónico" name="email" autoFocus />
+						</Grid>
+						<Grid item xs={12}>
+							<TextField required fullWidth name="password" label="Contraseña" type="password" id="password" />
+						</Grid>
+					</Grid>
+					<Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+						Registrate
+					</Button>
+					<Grid container justifyContent="flex-end">
+						<Grid item>
+							<Link onClick={() => navigate("/")} variant="body2">
+								¿Ya tiene una cuenta? Iniciar sesión
+							</Link>
+						</Grid>
+					</Grid>
+				</Box>
+			</Box>
+			<Copyright sx={{ mt: 5 }} />
+			<AlertDialog
+				open={openDialog}
+				handleClose={handleCloseDialog}
+				title="Registrate"
+				content={dialogContent}
+				label={dialogActionLabel}
+			/>{" "}
+		</Container>
+	);
 };
